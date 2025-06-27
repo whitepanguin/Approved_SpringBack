8bc6ea74b34364370775a32beef1b8e68a6379f5
whitepanguin <dun04045@gmail.com>
2025-06-27 13:51:26 +0900

    userid

--- src/main/java/com/example/backend/repository/UserRepository.java
+++ src/main/java/com/example/backend/repository/UserRepository.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 
 public interface UserRepository extends MongoRepository<User, String> {
     Optional<User> findByEmail(String email);
+    Optional<User> findByUserid(String userid);
     Optional<User> findByNameAndBirthDate(String name, String birthDate);
     Optional<User> findByEmailAndNameAndBirthDate(String email, String name, String birthDate);
     boolean existsByUserid(String userid);
